05efe262f71ebee9bca7f3223a1c501475c9856c
Muhammad Bintang <halilintar8@yahoo.com>
2025-09-01 23:33:59 +0700

    [bintang 12] change to version 2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
         mavenCentral()
     }
     plugins {
-        id("com.android.application") version "8.12.1"
+        id("com.android.application") version "8.12.2"
         id("org.jetbrains.kotlin.android") version "1.9.24"
         id("org.jetbrains.kotlin.kapt") version "1.9.24"
     }
